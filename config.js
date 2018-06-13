01bdcbd1447abb18797afbbe8a778f92120f0978
snyderth <38302855+snyderth@users.noreply.github.com>
2018-06-13 15:58:08 -0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'EhjgLdVVcWXrrS1T9M0bRqUvZ',
-    consumer_secret:'legqi8ZWpAPfKe55OVHuxpJFaJUOdS0RyqliLlvv9i2eGSeaQI',
-    access_token:'854455674268303360-xBURRuFihbkniAKG4MlQLaLMfjdZLPf',
-    access_token_secret:'rZlAYk33thzbRtDTUbB925EpwJRjMsIhRw3F4ErOIwEeE'
-};
